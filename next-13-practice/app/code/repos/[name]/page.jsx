761e7a4377023385496326878449423b2ed9c07c
Rahiful <rahifulislam@gmail.com>
2023-04-10 14:43:07 +0600

    Repo and Repodirs

--- next-13-practice/app/code/repos/[name]/page.jsx
+++ next-13-practice/app/code/repos/[name]/page.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import Link from "next/link"
+import Repo from "@/app/components/Repo"
 
 const RepoPage = ({ params: {name}}) => {
   return (
     <div className='card'>
-        <h2>{name}</h2>
-        <p>Repo details</p>
+      <Link href='/code/repos' className='btn btn-back'>Back to Repogitories</Link>
+        <Repo name={name}/>
     </div>
   )
 }
